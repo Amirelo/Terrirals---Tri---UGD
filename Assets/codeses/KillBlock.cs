81928ea07d24a08b7152a7048b5f8ddcc7748979
Amirelo <trandang210799@gmail.com>
2025-07-12 14:34:42 +0700

    Add files

---
+++ Assets/codeses/KillBlock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBlock : MonoBehaviour
+{
+    public Vector3 lapSart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("BSEDEDEDEDEFRGFSDFDRTBESTESRBEBGESSZbr");
+        transform.localScale = lapSart;
+        transform.rotation = Quaternion.Euler(45, 45, 45);
+        transform.position += new Vector3(0, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f) * Time.deltaTime;
+    }
+}
--- Assets/codeses/bullet.cs
+++ Assets/codeses/bullet.cs
@@ -8,12 +8,16 @@ public class bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
     }
 }
